3463627fe1980a63145f9502510ca2e8b05e4f59
think24 <think24@think24s-MacBook-Pro.local>
2017-05-12 17:30:53 +0530

    Login sample

--- LoginSample/LoginViewController.swift
+++ LoginSample/LoginViewController.swift
@@ -1,25 +1,107 @@
 //
-//  ViewController.swift
-//  LoginSample
+//  LoginViewController.swift
+//  HappiHive
 //
 //  Created by think24 on 5/12/17.
-//  Copyright © 2017 Think42Labs. All rights reserved.
+//  Copyright © 2017 SSB Software solutions. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class LoginViewController: UIViewController, UITextFieldDelegate
+{
+    //MARK:- Variables
+    //MARK:-- Outlet
+    
+    @IBOutlet weak var userNameTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    //MARK:-- Class
+    
+    //MARK:- View life cycle
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
+        
+        self.setInitialUISetUp()
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK:- Button action
+    @IBAction func loginButtonAction(_ sender: UIButton)
+    {
+        let userName = getTrimmedString(text: self.userNameTxtFld.text)
+        let password = getTrimmedString(text: self.passwordTxtFld.text)
+        if userName == "" || password == ""
+        {
+            showAlert(alertText : "Check your credentials and try again.")
+        }
+        else
+        {
+            self.checkLoginCredential(userName: userName, password: password)
+        }
+    }
+    
+    //MARK:- Text field
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        if textField == self.userNameTxtFld
+        {
+            self.passwordTxtFld.becomeFirstResponder()
+        }
+        return true
+    }
+    
+    //MARK:- Private function
+    
+    //MARK:-- UISetUp
+    
+    private func setInitialUISetUp()
+    {
+        
+    }
+    
+    private func saveUserData(userData : [String : Any])
+    {
+        
+    }
+    
+    //MARK:-- Service function
+    
+    private func checkLoginCredential(userName : String, password : String)
+    {
+        print(userName,password)
+        
+        showAlert(alertText : "User name " + userName + "  password " +  password )
+    }
+    
+    func getTrimmedString(text : String?) -> String
+    {
+        if text != nil
+        {
+            return text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }
+        return ""
+    }
+    
+    private func showAlert(alertText : String)
+    {
+        let alert = UIAlertController(title: "Alert", message: alertText, preferredStyle: UIAlertControllerStyle.alert)
+        let acceptButton = UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction) in
+            
+        })
+        alert.addAction(acceptButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
